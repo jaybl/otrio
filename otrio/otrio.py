7adc6cbdb83b40048028cd0fedd947aba8fa2388
Jay Lin <jaybl@uci.edu>
2018-11-15 14:26:10 -0600

    logic fixes
    now you must press new to start a game. a game over is finally a thing to, just click to get past.
    
    MAJOR BUG discovered: logic() thinks that a V formation (2 in the corners, 1 in the middle) is a victory royale, even if they're not straight. needs to fix.

--- otrio/otrio.py
+++ otrio/otrio.py
@@ -25,6 +25,7 @@ def callback():
 
 def pulverize(newtop : Toplevel):
     newtop.destroy()
+    start()
     newtop.grab_release()
     
 def command_new():
@@ -118,6 +119,9 @@ def find_closest(x,y):
 def test(event):
     print("test")
 
+def stop_everything(event):
+    back.delete("all")
+
 def onclick(event):
     item = back.find_closest(event.x, event.y) # for some reason item is a tuple lol
     tags = back.gettags(item)
@@ -127,14 +131,31 @@ def onclick(event):
         '''print(which, x,y)'''
         back.itemconfig(item, fill=colors[player])
         back.itemconfig(item, tags=("filled", which, x, y))
-        (logic())
+        if logic() ==1:
+            status.set("game over, player %d (%s) wins -- click to continue", player, colors[player])
+            back.bind('<Button-1>', stop_everything)
+            return
+        elif logic() == 2:
+            status.set("game over, no one wins -- click to continue")
+            back.bind('<Button-1>', stop_everything)
+            return
         inc_player()
         '''
     else:
         print(tags[0])
         '''
 #logic
+
+def no_moves():
+    items = back.find_all()
+    for i in items:
+        if back.gettags(i)[0] != "filled":
+            return 0
+    return 1
+    
 def logic():
+    if no_moves() == 1:
+        return 2
     filled = back.find_withtag("filled")
     '''print(filled)'''
     #On^3 to search for matches
@@ -147,8 +168,16 @@ def logic():
             if i != item and player_color == back.itemcget(i, "fill"):
                 tags = back.gettags(i)
                 r, c = (int)(tags[2]), (int)(tags[3])
+                #all 3 in 1 cell check
+                if c == y and r == x:
+                    for var in filled:
+                        subtags = back.gettags(var)
+                        if var != item and var != i:
+                            if which != subtags[1] and subtags[1] != tags[1] and (int)(subtags[2]) == x and (int)(subtags[3]) == y and back.itemcget(var, "fill") == player_color:
+                                print("MATCH")
+                                return 1
                 #vertical check
-                if y-1 >=0 and y-1 == c: 
+                if y-1 == c: 
                     if which == tags[1]: #same size type
                         for var in filled:
                             subtags = back.gettags(var)
@@ -164,7 +193,7 @@ def logic():
                                     print("MATCH")
                                     return 1
                 #horizontal check
-                if x-1 >=0 and x-1 == r:
+                if x-1 == r:
                     if which == tags[1]: #same size type
                         for var in filled:
                             subtags = back.gettags(var)
@@ -180,7 +209,7 @@ def logic():
                                     print("MATCH")
                                     return 1
                 #diagonal check, down right
-                if x-1 >=0 and x-1 == r and y-1 >=0 and y-1 == c:
+                if x==1 and y==1: 
                     if which == tags[1]: #same size type
                         for var in filled:
                             subtags = back.gettags(var)
@@ -196,7 +225,7 @@ def logic():
                                     print("MATCH")
                                     return 1
                 #diagonal check, down left
-                if x-1 >=0 and x-1 == r and y+1 >=0 and y+1 == c:
+                if x == 1 and y == 1:
                     if which == tags[1]: #same size type
                         for var in filled:
                             subtags = back.gettags(var)
@@ -216,50 +245,26 @@ def logic():
     print("---------------")
     
 #drawing grid
-back.pack()
-shapes = []
-width = back.winfo_width()/3
-height = back.winfo_height()/3
-for r in range(3):
-    for c in range(3):
-        a = back.create_rectangle(r*width, c*height, (r+1)*width, (c+1)*height, tags=("rect",r,c), fill="white")
-        shapes.append(a)        
-        b = back.create_oval(r*width+3, c*height+3, (r+1)*width-3, (c+1)*height-3, tags=("outer",r,c), fill="white")
-        shapes.append(b)
-        d = back.create_oval(r*width+35, c*height+35, (r+1)*width-35, (c+1)*height-35, tags=("mid",r,c), fill="white")
-        shapes.append(d)
-        e = back.create_oval(r*width+75, c*height+65, (r+1)*width-75, (c+1)*height-65, tags=("inner",r,c), fill="white")
-        shapes.append(e)
+
+def start():
+    back.pack()
+    shapes = []
+    width = back.winfo_width()/3
+    height = back.winfo_height()/3
+    for r in range(3):
+        for c in range(3):
+            a = back.create_rectangle(r*width, c*height, (r+1)*width, (c+1)*height, tags=("rect",r,c), fill="white")
+            shapes.append(a)        
+            b = back.create_oval(r*width+3, c*height+3, (r+1)*width-3, (c+1)*height-3, tags=("outer",r,c), fill="white")
+            shapes.append(b)
+            d = back.create_oval(r*width+35, c*height+35, (r+1)*width-35, (c+1)*height-35, tags=("mid",r,c), fill="white")
+            shapes.append(d)
+            e = back.create_oval(r*width+75, c*height+65, (r+1)*width-75, (c+1)*height-65, tags=("inner",r,c), fill="white")
+            shapes.append(e)
 
 
-back.bind('<Button-1>', onclick)
+    back.bind('<Button-1>', onclick)
 
-
-'''
-#button grid in the frame
-
-buttons = {}
-
-for r in range(1,4):
-    for c in range(1,4):                       
-        back.grid_columnconfigure(r, weight=1)
-        back.grid_columnconfigure(c, weight=1)
-        
-        btn = Square(r, c, master=back, height=10, width=10,)
-        btn.grid(row=r,column=c, sticky="news")
-        btn.config(command=btn.showCoords)
-
-        buttons[(r,c)] = btn
-
-#adding weights to grid
-back.grid_columnconfigure(0, weight=1)
-back.grid_columnconfigure(4, weight=1)
-back.grid_rowconfigure(0, weight=1)
-back.grid_rowconfigure(4, weight=1)
-back.grid_propagate(False)
-
-window.update()
-'''
         
 #window display
 window.mainloop()
